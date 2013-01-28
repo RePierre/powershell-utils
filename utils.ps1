01db1903b8418742a8d81ddce4bc2e88789531cb
Rebeja Petru <petru.rebeja@gmail.com>
2013-01-28 10:02:18 +0200

    Initial commit

---
+++ utils.ps1
@@ -0,0 +1,26 @@
+﻿function Get-ScriptDirectory
+{
+	$invocation = (Get-Variable MyInvocation -Scope 1).Value
+	Split-Path $invocation.MyCommand.Path
+}
+
+function Deploy-Files($sourceDir, $destinationDir, $filter)
+{
+	Write-Host "Searching in: " $sourceDir
+	$files = Get-ChildItem -LiteralPath $sourceDir -Filter $filter
+	ForEach($file in $files)
+	{
+		Write-Host "Copying file: " $file
+		Copy-Item -LiteralPath (Join-Path $sourceDir $file) -Destination $destinationDir
+	}
+}
+
+function IE-OpenUrls($inputFile)
+{
+	$iexplore = "iexplore.exe"
+	$urls = Get-Content $inputFile
+	foreach($url in $urls)
+	{
+    	start-process $iexplore $url
+	}
+}
